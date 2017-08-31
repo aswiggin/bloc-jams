548aee6dbea11573324e2b4f3e5b2b3dd6fa07a4
Andrew Wiggins <aswiggin@ncsu.edu>
2017-08-31 16:14:10 -0400

    fixed album page

--- scripts/album.js
+++ scripts/album.js
@@ -123,7 +123,6 @@ var clickHandler = function(targetElement) {
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 
 var songRows = document.getElementsByClassName('album-view-song-item');
-
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
